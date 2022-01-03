dbf0bc5a74813e670915697a22f980162f5bd685
QambarAli1 <qamberali4904@gmail.com>
2022-01-03 16:58:05 +0500

    updated

--- src/home.jsx
+++ src/home.jsx
@@ -1,9 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container , Row , Col } from 'react-bootstrap';
+import img1 from './ecom.png'
 
 function Home() {
   return (
     <Container>
+      <Row>
+        <Col>
+          <img src={img1} width="100%" />
+        </Col>
+      </Row>
       <Row>
         <Col>
           <h2 className='welcome'>Welcome to SamanChahiye.pk</h2>
