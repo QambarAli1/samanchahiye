8fb22e807f4def191544314d55f186edeac0606a
QambarAli1 <qamberali4904@gmail.com>
2022-01-10 00:31:18 +0500

    updated

--- src/benefits.jsx
+++ src/benefits.jsx
@@ -1,10 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 
 function Benefits(props) {
   return (
     <>
-        <div>
+        <div className='benefits-component'>
+            {/* <LocalShippingIcon /> */}
+            <p className='benicons'>{props.icon}</p>
             <h2>  {props.heading} </h2>
             <p> {props.para} </p>
         </div>
--- src/home.jsx
+++ src/home.jsx
@@ -4,6 +4,9 @@ import img1 from './ecom.png';
 import img2 from './online_shopping.jpeg';
 import './App.css'
 import Benefits from './benefits';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import VerifiedIcon from '@mui/icons-material/Verified';
+import DeliveryDiningIcon from '@mui/icons-material/DeliveryDining';
 
 function Home() {
   return (
@@ -28,7 +31,14 @@ function Home() {
         <h3>Delievering qulality products all over Pakistan</h3>
         <p>Free Delivery scroos Karachi</p>
       </div>
-      <Benefits heading="Free Delivery" para="Across Karachi" />
+      <div>
+        <h1 className='whyshopwithus'>Why Shop With Us</h1>
+      </div>
+      <div className="benefits-maindiv">
+        <Benefits icon={<DeliveryDiningIcon />} heading="Free Delivery" para="Providing you free dilivery across Karachi" />
+        <Benefits icon={<LocalShippingIcon />} heading="Free Shipping" para="Providing you free Shipping across Karachi" />
+        <Benefits icon={<VerifiedIcon />} heading="Best Quality" para="Providing you best quality of products" />
+      </div>
     </div>
 
   );
